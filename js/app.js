f5bf852794c2ba3a2b94370464a0f818f11c0f37
azrappp <118091170+azrappp@users.noreply.github.com>
2023-06-30 09:51:47 +0700

    penambahan transisi

--- js/app.js
+++ js/app.js
@@ -8,5 +8,5 @@ const observer = new IntersectionObserver((entries) => {
     }
   });
 });
-const hiddenElements = document.querySelectorAll(".content");
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
